f18f5297b334ec000f6c966a2ee5a53205576648
Enzo-Martolini <enzo.martolini@laplateforme.io>
2024-05-26 20:29:35 +0200

    models book faits

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const connectDB = require("../config/db");
+const connectDB = require("./config/db");
 const app = express();
 require('dotenv').config();
+
+//Connecion à la db
+connectDB();
 
 // Middleware qui permet de traiter les données de la requête
 app.use(express.json());
@@ -10,8 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 // Utilisation des routes définies dans post.routes.js
 app.use("/post", require("./routes/post.routes"));
 
-//Connecion à la db
-connectDB();
 
 // Lancer le serveur
 app.listen(3000, () => console.log("Le serveur a démarré au port 3000"));
